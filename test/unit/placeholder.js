223cc3aa18a5acf3fab9bada2c926a9a479bf78a
Randy Tarampi <rt@randytarampi.ca>
2018-07-25 21:55:51 -0700

    Add CI to the `me` monorepo.
    Look at that test coverage. Yikes.

---
+++ test/unit/placeholder.js
@@ -0,0 +1,8 @@
+import {expect} from "chai";
+import {describe, it} from "mocha";
+
+describe("placeholder", () => {
+    it("can run tests", () => {
+        expect(true).to.eql(true);
+    });
+});
